4b599e5bafc9677b2f6cd0038de57a2f8299ade9
dubey-shivani <shreyamishra040398@gmail.com>
2021-11-10 23:46:59 +0530

    comitted

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -47,50 +47,53 @@ const Home = ({
   const arr = JSON.parse(localStorage.getItem("userInfo"));
   return (
     <Container>
-      <div className='main-div'>
-        <div className='subheader'>
-          <h3 className='subtitle'>
+      <div className="main-div">
+        <div className="subheader">
+          <h3 className="subtitle">
             Choose one from the categories below and see how many questions you
             can answer correctly out of 10 questions!
           </h3>
         </div>
 
-        <div className='categories'>
-          <div className='circular-figure__category-name'>
-            <div className='music__circle'>
+        <div className="categories">
+          <div className="circular-figure__category-name">
+            <div className="music__circle">
               <span onClick={selectedCategoryFilm}>
                 <img
-                  className='images'
-                  src='http://devinardya-quizmaster.surge.sh/static/media/movie-icon.719897dc.svg'
+                  className="images"
+                  src="http://devinardya-quizmaster.surge.sh/static/media/movie-icon.719897dc.svg"
                 />
               </span>
             </div>
 
-            <h2 className='category__name'>Movie</h2>
+            <h2 className="category__name">Movie</h2>
           </div>
-          <div className='circular-figure__category-name'>
-            <div className='music__circle'>
+          <div className="circular-figure__category-name">
+            <div className="music__circle">
               <span onClick={selectedCategoryMusic}>
                 <img
-                  className='images'
-                  src='http://devinardya-quizmaster.surge.sh/static/media/music-icon.469e959f.svg'
+                  className="images"
+                  src="http://devinardya-quizmaster.surge.sh/static/media/music-icon.469e959f.svg"
                 />
               </span>
             </div>
-            <h2 className='category__name'>Music</h2>
+            <h2 className="category__name">Music</h2>
           </div>
-          <div className='circular-figure__category-name'>
-            <div className='music__circle'>
+          <div className="circular-figure__category-name">
+            <div className="music__circle">
               <span onClick={selectedCategoryComputer}>
                 <img
-                  className='images'
-                  src='http://devinardya-quizmaster.surge.sh/static/media/books-icon.3b876aa2.svg'
+                  className="images"
+                  src="http://devinardya-quizmaster.surge.sh/static/media/books-icon.3b876aa2.svg"
                 />
               </span>
             </div>
-            <h2 className='category__name'>Book</h2>
+            <h2 className="category__name">Book</h2>
           </div>
         </div>
+        <button className="submit__button" onClick={startQuiz}>
+          Start Quiz
+        </button>
         {/* {arr ?
 
 
@@ -99,9 +102,6 @@ const Home = ({
             </button> : <button className="submit__button" onClick={handleOpen}>
                         Register
             </button>} */}
-        <button className='submit__button' onClick={startQuiz}>
-          Start Quiz
-        </button>
 
         <ComponentModal open={open} setOpen={setOpen} />
       </div>
