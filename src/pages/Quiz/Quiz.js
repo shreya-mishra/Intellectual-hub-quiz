e479203e8be14053e5250bedc988ad176a2b5696
SHREYA MISHRA <61110378+shreya-mishra@users.noreply.github.com>
2021-11-24 20:32:14 +0530

    Update Quiz.js
    Resolved bugs

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,62 +1,63 @@
-import { CircularProgress } from '@material-ui/core';
-import React, { useEffect, useState } from 'react'
-import { Questions } from '../../components/Questions/Questions';
+import { CircularProgress } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Questions } from "../../components/Questions/Questions";
 import "./Quiz.css";
 const Quiz = ({ name, score, questions, setQuestions, setScore }) => {
-    const [options, setOptions] = useState();
-    const [currQues, setCurrQues] = useState(0)
-    // const [userName, setUserName] = useState("")
-    var arr = JSON.parse(localStorage.getItem('userInfo'));
-    console.log("sssssssssssssssss", arr)
+  const [options, setOptions] = useState();
+  const [currQues, setCurrQues] = useState(0);
+  // const [userName, setUserName] = useState("")
+  var arr = JSON.parse(localStorage.getItem("userInfo"));
+  //   console.log("sssssssssssssssss", arr);
 
+  useEffect(() => {
+    console.log("Questionsss", questions);
 
-    useEffect(() => {
-        console.log(questions);
-
-
-        setOptions(questions && handleShuffle([questions[currQues]?.correct_answer,
-        ...questions[currQues]?.incorrect_answers,
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currQues]?.correct_answer,
+          ...questions[currQues]?.incorrect_answers,
         ])
-        );
-    }, [questions, currQues]);
-    console.log(options);
+    );
+  }, [questions, currQues]);
+  console.log(options);
 
-    const handleShuffle = (optionss) => {
-        return optionss.sort(() => Math.random() - 0.5);
+  const handleShuffle = (optionss) => {
+    return optionss.sort(() => Math.random() - 0.5);
+  };
+  return (
+    <div className='quiz'>
+      <span className='subTitle' style={{ color: "white" }}>
+        {arr ? `Welcome, ${arr.name}` : "Welcome "}
+      </span>
 
+      {questions ? (
+        <>
+          <div className='quizInfo'>
+            <span>{questions[currQues].category}</span>
+            <span>Score : {score}</span>
+          </div>
+          <Questions
+            currQues={currQues}
+            setCurrQues={setCurrQues}
+            questions={questions}
+            options={options}
+            correct={questions[currQues]?.correct_answer}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />
+        </>
+      ) : (
+        <CircularProgress
+          style={{ margin: 100 }}
+          color='inherit'
+          size={150}
+          thickness={1}
+        />
+      )}
+    </div>
+  );
+};
 
-    }
-    return (
-        <div className="quiz">
-            <span className="subTitle" style={{ color: "white" }}>
-                {arr ? `Welcome, ${arr.name}` : "Welcome "}
-            </span>
-
-            {
-                questions ? (
-                    <>
-                        <div className="quizInfo">
-                            <span >{questions[currQues].category}</span>
-                            <span>Score : {score}</span>
-                        </div>
-                        <Questions
-                            currQues={currQues}
-                            setCurrQues={setCurrQues}
-                            questions={questions}
-                            options={options}
-                            correct={questions[currQues]?.correct_answer}
-                            score={score}
-                            setScore={setScore}
-                            setQuestions={setQuestions}
-                        />
-                    </>
-
-                ) : (
-                    <CircularProgress style={{ margin: 100 }} color="inherit" size={150} thickness={1} />
-                )
-            }
-        </div>
-    )
-}
-
-export default Quiz
+export default Quiz;
